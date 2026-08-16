d7b6931b960131c538655b95af06c7f7d8a40d34
aleray0saslal <t2wmt6sgff@privaterelay.appleid.com>
2026-08-16 16:17:03 +0200

    Actualizar ContentView.swift

--- SwiftPlaygrounds/ContentView.swift
+++ SwiftPlaygrounds/ContentView.swift
@@ -1,1 +1,12 @@
-a
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
