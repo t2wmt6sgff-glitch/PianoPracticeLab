b5014d73fcdc1712926f0ca03fd5cc2efaf58aa7
aleray0saslal <t2wmt6sgff@privaterelay.appleid.com>
2026-08-16 18:54:09 +0200

    Actualizar ContentView.swift

--- SwiftPlaygrounds/ContentView.swift
+++ SwiftPlaygrounds/ContentView.swift
@@ -5,11 +5,22 @@ struct ContentView: View {
     let container: ModelContainer
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            TodayView(container: container)
+                .tabItem {
+                    Label("Hoy", systemImage: "calendar.today")
+                }
+            
+            Text("Vista de Calendario (Próximamente)")
+                .tabItem {
+                    Label("Calendario", systemImage: "calendar")
+                }
+            
+            Text("Vista de Configuración (Próximamente)")
+                .tabItem {
+                    Label("Configuración", systemImage: "gear")
+                }
         }
     }
 }
+
